8d4b0dbcef57273390f91f81e0c85f20849f7bfa
yassin1712hh <yassinglovo8@gmail.com>
2025-09-04 18:59:24 +0100

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -349,7 +349,34 @@ export const products: Product[] = [
     category: 'Grow a Garden',
     image: 'https://images-ext-1.discordapp.net/external/tr_fXfqkxiWWY7iCoQAjlLZzoOLXJQvZ6hg8e0x6KsY/https/i.imgur.com/duHIkR5h.jpg?format=webp&width=846&height=846',
     description: 'coruupted ',
-    price: 5000,
+    price: 1000,
+    currency: 'IQD'
+  },
+    {
+      id: 'gg-14',
+    name: 'Kitsune ',
+    category: 'Grow a Garden',
+    image: 'https://images-ext-1.discordapp.net/external/3zhCy95CF4WeGH3J7E7zoZDKQiopbjQ-FPWPJcweNWA/https/i.imgur.com/KJABhc0h.jpg?format=webp&width=846&height=846',
+    description: '. ',
+    price: 25000,
+    currency: 'IQD'
+  },
+    {
+      id: 'gg-15',
+    name: 'mega kitsune',
+    category: 'Grow a Garden',
+    image: 'https://images-ext-1.discordapp.net/external/tr_fXfqkxiWWY7iCoQAjlLZzoOLXJQvZ6hg8e0x6KsY/https/i.imgur.com/duHIkR5h.jpg?format=webp&width=846&height=846',
+    description: 'coruupted ',
+    price: 30000,
+    currency: 'IQD'
+  },
+  {
+    id: 'gg-16',
+    name: 'rainbow kitsune',
+    category: 'Grow a Garden',
+    image: 'https://images-ext-1.discordapp.net/external/tr_fXfqkxiWWY7iCoQAjlLZzoOLXJQvZ6hg8e0x6KsY/https/i.imgur.com/duHIkR5h.jpg?format=webp&width=846&height=846',
+    description: 'coruupted ',
+    price: 30000,
     currency: 'IQD'
   },
 ];
